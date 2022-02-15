61b63009677e953aa92da7fd14ae03618315c39c
Ivan <canelo_1987_@hotmail.com>
2022-02-15 17:33:08 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { FooterDesktop } from './components/footer/footer-desktop';
 import { HeaderHome } from './components/movies-home/header-home';
 import { NavDesktop } from './components/nav/nav-desktop';
 
 
+
 function App() {
+
+
   return (
     <div className="App">
       <NavDesktop />

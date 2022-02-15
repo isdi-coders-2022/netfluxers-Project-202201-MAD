249570f3f15eae42de9ff9df4810d81c7e25ec40
Felipe Luca de Tena <felipelucadetena@gmail.com>
2022-02-15 12:41:48 +0100

    Added home jsx and api calls

---
+++ src/services/apiTmdb.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export function getDetail(idmovie) {
+  return axios.get(
+    `https://api.themoviedb.org/3/movie/${idmovie}?api_key=4b4f416a1b778e7770adf119452664ae`
+  );
+}
+export function getCast(idmovie) {
+  return axios.get(
+    `http://api.themoviedb.org/3/movie/${idmovie}/credits?api_key=4b4f416a1b778e7770adf119452664ae`
+  );
+}

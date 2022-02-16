21aa4fc1919c8d67f93e9f29010e2cb3d1a681c6
Ivan <canelo_1987_@hotmail.com>
2022-02-16 13:34:07 +0100

    Create Api Local

--- src/components/favourites/fav-movie.js
+++ src/components/favourites/fav-movie.js
@@ -1,24 +1,54 @@
 import './style-fav-list.scss';
+import { useEffect,useState } from 'react';
+import { GetAllLocalFav } from '../../services/apiLocal';
+
 
 export function FavoriteMovie() {
-  return (
-    <div className="movie">
-      <img
-        className="movie__poster"
-        src="https://image.tmdb.org/t/p/w1280/HORpg5CSkmeQlAolx3bKMrKgfi.jpg"
-        alt=""
-      />
+  const [movieFav, setMovieFav] = useState();
 
-      <div className="movie-stars">
-        <i className="movie__stars stars-1">⭐</i>
-        <i className="movie__stars stars-2">⭐</i>
-        <i className="movie__stars stars-3">⭐</i>
-        <i className="movie__stars stars-4">⭐</i>
-        <i className="movie__stars stars-5">⭐</i>
-      </div>
-      <span className="movie__title">James Bond Collection</span>
-      <span className="movie__genred">Crime</span>
-      <span className="item__duration">2h 30min</span>
+  useEffect(() => {
+    GetAllLocalFav().then((result) => {
+      setMovieFav(result.data);
+      console.log(movieFav);
+    });
+  }, []);
+
+  return(
+    <div className="movie">
+    <img
+      className="movieposter"
+      src="https://image.tmdb.org/t/p/w1280/HORpg5CSkmeQlAolx3bKMrKgfi.jpg"
+      alt=""
+    />
+
+    <div className="movie-stars">
+      <i className="moviestars stars-1">⭐</i>
+      <i className="moviestars stars-2">⭐</i>
+      <i className="moviestars stars-3">⭐</i>
+      <i className="moviestars stars-4">⭐</i>
+      <i className="moviestars stars-5">⭐</i>
     </div>
-  );
-}
+    <span className="movietitle">James Bond Collection</span>
+    <span className="moviegenred">Crime</span>
+    <span className="item__duration">2h 30min</span>
+  </div>
+  )
+
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

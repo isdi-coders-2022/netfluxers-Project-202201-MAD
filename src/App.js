cc54510e700c753673fc27cef13bb741e0d6ebea
Jesus Rodriguez <jesusrodriguezgonzalezgr@gmail.com>
2022-02-17 20:52:03 +0100

    Added various changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavDesktop } from './components/nav/nav-desktop';
 import { ContainerHome } from './components/container-home/container-home';
 import { ContainerFav } from './components/container-fav/container-fav';
 import { ContainerDetails } from './components/container-details/container-details';
+import { DetailsTrailer } from './components/details-web/details-trailer';
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
         <Route path="/" element={<ContainerHome />} />
         <Route path="/favorites-list" element={<ContainerFav />} />
         <Route path="/details/:id" element={<ContainerDetails />} />
+        <Route path="/trailer/:id" element={<DetailsTrailer />} />
         <Route path="*" element={<ContainerHome />} />
       </Routes>
       <FooterDesktop />

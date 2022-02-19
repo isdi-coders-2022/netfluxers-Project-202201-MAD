97568ad4ed461583eb6fdcdb393bc1bf00be847b
Jesus Rodriguez <jesusrodriguezgonzalezgr@gmail.com>
2022-02-19 17:56:32 +0100

    Added test fav-movie

--- src/components/favourites/fav-movie.spec.js
+++ src/components/favourites/fav-movie.spec.js
@@ -1,14 +1,44 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { getMovieGenres } from '../../services/apiTmdb';
 import { FavoriteMovie } from './fav-movie';
 
-test('Render FavoriteMovie', () => {
-  render(<FavoriteMovie />);
-  const title = screen.getByText(/james bond collection/i);
-  expect(title).toBeInTheDocument();
-  const image = screen.getByRole('img');
-  expect(image).toBeInTheDocument();
-  const genero = screen.getByText(/crime/i);
-  expect(genero).toBeInTheDocument();
-  const time = screen.getByText(/2h 30min/i);
-  expect(time).toBeInTheDocument();
+// test('Render GenredMovie', () => {
+//   render(<GenredMovie />);
+
+//   const figure = screen.getByTestId('figure');
+//   const figcaption = screen.getByTestId('figcaption');
+//   const poster = screen.getByTestId('poster');
+//   expect(figcaption).toBeInTheDocument();
+//   expect(figure).toBeInTheDocument();
+//   expect(poster.src).toContain('https://image.tmdb.org/t/p/w1280/');
+// });
+
+const movie = {
+  data: {
+    results: [
+      {
+        id: 555,
+        title: 'The Amazing Spider',
+        poster_path: '/ay7xwXn1G9fzX9TUBlkGA584rGi.jpg',
+      },
+    ],
+  },
+};
+
+jest.mock('../../services/apiTmdb');
+
+describe('first getMovieGenres from API', () => {
+  beforeEach(() => {
+    getMovieGenres.mockResolvedValue(movie);
+  });
+  test('should show Genres-', () => {
+    render(
+      <BrowserRouter>
+        <FavoriteMovie movie={movie} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText(/Udpate/i)).toBeInTheDocument();
+  });
 });

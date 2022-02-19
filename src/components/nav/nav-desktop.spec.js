c7d3168fceda0bd9a9244bc22195553f10b54c64
Felipe Luca de Tena <felipelucadetena@gmail.com>
2022-02-19 16:17:24 +0100

    added nav desktop test

--- src/components/nav/nav-desktop.spec.js
+++ src/components/nav/nav-desktop.spec.js
@@ -1,12 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { NavDesktop } from './nav-desktop';
 
-test('Render NavDesktop', () => {
-  render(<NavDesktop />);
-  const liHome = screen.getByText(/Home/i);
-  const liFavorites = screen.getByText(/Favorites/i);
-  const liAccount = screen.getByText(/Account/i);
-  expect(liHome).toBeInTheDocument();
-  expect(liFavorites).toBeInTheDocument();
-  expect(liAccount).toBeInTheDocument();
+test('renders Menu list', () => {
+  const navOptions = [
+    { path: '/', label: 'Home' },
+    { path: '/favorites-list', label: 'Favorites' },
+  ];
+  render(
+    <BrowserRouter>
+      <NavDesktop navOptions={navOptions} />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
 });
